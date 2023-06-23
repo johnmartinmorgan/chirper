cc6dba236b6930ac12cc6350324974422c5272f0
johnmartinmorgan <56900625+johnmartinmorgan@users.noreply.github.com>
2023-06-23 02:01:08 -0300

    05. Editing Chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -35,14 +35,22 @@ class ChirpController extends Controller
         //
     }
 
-    public function edit(Chirp $chirp)
+    public function edit(Chirp $chirp): View
     {
-        //
+        $this->authorize('update', $chirp);
+        return view('chirps.edit', [
+            'chirp' => $chirp,
+        ]);
     }
 
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
-        //
+        $this->authorize('update', $chirp);
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+        $chirp->update($validated);
+        return redirect(route('chirps.index'));
     }
 
     public function destroy(Chirp $chirp)
